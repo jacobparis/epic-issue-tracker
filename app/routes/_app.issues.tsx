76bcc33d949eca2a260cde0eaa8905a3054e6986
Jacob Paris <jacob@jacobparis.com>
2023-12-27 18:22:38 +0700

    feature: build new item form

--- app/routes/_app.issues.tsx
+++ app/routes/_app.issues.tsx
@@ -1,14 +1,43 @@
-import { type MetaFunction } from '@remix-run/node'
+import { type MetaFunction, type DataFunctionArgs, json } from '@remix-run/node'
+import { Form } from '@remix-run/react'
+import { Field } from '#app/components/forms.tsx'
+import { Button } from '#app/components/ui/button.tsx'
 
 export const meta: MetaFunction = () => [
 	{
 		title: 'Issues',
 	},
 ]
+
+export async function action({ request }: DataFunctionArgs) {
+	return json({})
+}
+
 export default function Issues() {
 	return (
-		<div className="grid h-full place-items-center py-20">
-			<h1 className="text-5xl font-bold">Issues</h1>
+		<div className="mx-auto max-w-4xl p-4">
+			<div className="mt-8">
+				<Form method="POST">
+					<Field
+						labelProps={{ children: 'Title' }}
+						inputProps={{
+							type: 'text',
+							name: 'title',
+							required: true,
+						}}
+					/>
+
+					<Field
+						labelProps={{ children: 'Description' }}
+						inputProps={{
+							type: 'text',
+							name: 'description',
+						}}
+					/>
+
+					<Button type="submit">Submit</Button>
+				</Form>
+			</div>
 		</div>
 	)
 }
