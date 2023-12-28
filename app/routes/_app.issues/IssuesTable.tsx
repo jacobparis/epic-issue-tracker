f34219110fdf721893c4ced0441a0f2eb63d590f
Jacob Paris <jacob@jacobparis.com>
2023-12-28 17:17:39 +0700

    feature: display items in a table

--- app/routes/_app.issues/IssuesTable.tsx
+++ app/routes/_app.issues/IssuesTable.tsx
@@ -1,21 +1,126 @@
 import { type Issue } from '@prisma/client'
 import { type SerializeFrom } from '@remix-run/node'
 
+import {
+	useReactTable,
+	type ColumnDef,
+	getCoreRowModel,
+	flexRender,
+} from '@tanstack/react-table'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from '#app/components/ui/table.tsx'
+
 type IssueRow = Pick<
 	SerializeFrom<Issue>,
-	'id' | 'number' | 'title' | 'status' | 'priority'
+	'id' | 'number' | 'title' | 'status' | 'priority' | 'createdAt'
 >
+
+export const columns: ColumnDef<IssueRow>[] = [
+	{
+		accessorKey: 'number',
+		header: '#',
+	},
+	{
+		header: 'Title',
+		accessorKey: 'title',
+		cell({ row }) {
+			return <span className="font-medium">{row.getValue('title')}</span>
+		},
+	},
+	{
+		accessorKey: 'priority',
+		header: 'Priority',
+	},
+	{
+		accessorKey: 'status',
+		header: 'Status',
+	},
+	{
+		accessorKey: 'createdAt',
+		header: 'Created',
+		accessorFn(row) {
+			return new Date(row.createdAt).toLocaleDateString('en-US', {
+				month: 'short',
+				day: 'numeric',
+				year: 'numeric',
+			})
+		},
+		cell({ row }) {
+			return (
+				<span className="whitespace-nowrap">{row.getValue('createdAt')}</span>
+			)
+		},
+	},
+]
+
 export function IssuesTable({ issues }: { issues: Array<IssueRow> }) {
+	const table = useReactTable({
+		data: issues,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	})
+
 	return (
 		<div>
-			{issues.map(issue => (
-				<div key={issue.id} className="flex border-b">
-					<div className="px-4 py-2 align-middle">{issue.number}</div>
-					<div className="w-full px-4 py-2 align-middle">{issue.title}</div>
-					<div className="px-4 py-2 align-middle">{issue.status}</div>
-					<div className="px-4 py-2 align-middle">{issue.priority}</div>
-				</div>
-			))}
+			<Table>
+				<TableHeader>
+					{table.getHeaderGroups().map(headerGroup => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map(header => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder
+											? null
+											: flexRender(
+													header.column.columnDef.header,
+													header.getContext(),
+											  )}
+									</TableHead>
+								)
+							})}
+						</TableRow>
+					))}
+				</TableHeader>
+
+				<colgroup>
+					{/* Make the title column take up the rest of available space */}
+					{table.getAllColumns().map(column => (
+						<col
+							key={column.id}
+							className={column.id === 'title' ? 'w-full' : 'w-0'}
+						/>
+					))}
+				</colgroup>
+
+				<TableBody>
+					{table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map(row => (
+							<TableRow
+								key={row.id}
+								data-state={row.getIsSelected() && 'selected'}
+							>
+								{row.getVisibleCells().map(cell => (
+									<TableCell key={cell.id}>
+										{flexRender(cell.column.columnDef.cell, cell.getContext())}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns.length} className="h-24 text-center">
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
 		</div>
 	)
 }
